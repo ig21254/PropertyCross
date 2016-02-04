5dfb88de60f441ecf386b035dc5dda6cacd1034e
Master <master@iMac-de-LaSalleBCN-3.local>
2016-02-04 19:12:06 +0100

    Hecha gestion de orientacion en resultados de propiedades. Falta testear.

--- app/src/main/java/com/lasalle/second/part/propertycross/activities/ResultsContainerActivity.java
+++ app/src/main/java/com/lasalle/second/part/propertycross/activities/ResultsContainerActivity.java
@@ -1,12 +1,15 @@
 package com.lasalle.second.part.propertycross.activities;
 
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lasalle.second.part.propertycross.R;
+import com.lasalle.second.part.propertycross.fragments.ProfileContentFragment;
 import com.lasalle.second.part.propertycross.fragments.SearchResultContainerFragment;
 import com.lasalle.second.part.propertycross.fragments.SearchResultListFragment;
 import com.lasalle.second.part.propertycross.fragments.SearchResultMapFragment;
@@ -18,21 +21,51 @@ public class ResultsContainerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_container);
 
-        addResultListFragment();
+        addResultListFragment(getResources().getConfiguration().orientation);
     }
 
-    protected void addResultListFragment() {
+    protected void addResultListFragment(int orientation) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction =
                 fragmentManager.beginTransaction();
 
-        //SearchResultContainerFragment containerFragment = new SearchResultContainerFragment();
-        SearchResultMapFragment containerFragment = new SearchResultMapFragment();
+        Fragment containerFragment;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            containerFragment = new SearchResultMapFragment();
+        }
+        else {
+            containerFragment = new SearchResultContainerFragment();
+        }
+
         fragmentTransaction.add(
                 R.id.result_container_layout,
                 containerFragment);
         fragmentTransaction.commit();
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
+        updateResultListFragment(newConfig.orientation);
+    }
+
+    protected void updateResultListFragment(int orientation) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+
+        Fragment containerFragment;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            containerFragment = new SearchResultMapFragment();
+        }
+        else {
+            containerFragment = new SearchResultContainerFragment();
+        }
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.result_container_layout, containerFragment);
+        transaction.commit();
+    }
 }
